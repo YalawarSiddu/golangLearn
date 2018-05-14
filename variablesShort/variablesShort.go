22c89b04401a934bb3376b0c55479a40f56d6014
Siddu Yalawar <yalawar.siddu@yahoo.com>
2018-05-14 10:43:58 +0530

    declare and use variables in short form

---
+++ variablesShort/variablesShort.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    intVar01 := 101
+    intVar02, strVar01 := 102, "TestString"
+
+    fmt.Printf("variables values, intVar01 %d, intVar02 %d, strVar01 %s", intVar01, intVar02, strVar01)
+}
